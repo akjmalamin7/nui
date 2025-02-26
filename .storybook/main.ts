ee49e125725a1e4c9ddb8a978547b75b07b7cede
akjmalamin7 <thpi205526@gmail.com>
2025-02-26 12:34:00 +0600

    init

---
+++ .storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    {
+      "name": "@storybook/addon-essentials",
+      "options": {
+        "docs": {
+          autodocs:"tag"
+        }
+      }
+    },
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions"
+  ],
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
+};
+export default config;
